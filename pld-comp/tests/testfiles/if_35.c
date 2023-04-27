9426f92174aae943f5db420206a1dde16196284b
Minh Ngo <bbbbbb267@gmail.com>
2023-04-27 18:58:29 +0200

    ajouter test if_35 : if compose

---
+++ pld-comp/tests/testfiles/if_35.c
@@ -0,0 +1,14 @@
+int main() {
+    int a = 0;
+    int b = 0;
+    if(a < 1){
+        b = 5;
+    } else {
+        if(b == 5) {
+            a = 1;
+        } else {
+            a = 2;
+        }
+    }
+    return a;
+}
