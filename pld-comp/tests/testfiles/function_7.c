81821569adb3c6969d273444e73f62498037cd9f
HenriS67 <henrib2001@gmail.com>
2023-04-28 01:57:09 +0200

    a regler (plsuieurs fonctions)

---
+++ pld-comp/tests/testfiles/function_7.c
@@ -0,0 +1,17 @@
+int add(int a, int b,int c,int d)
+{
+    return a + b + d * c - 4 ;
+}
+int mul(int a, int b,int c,int d)
+{
+    return a * b * d * c - 4 ;
+}
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = mul(a, b,c,d);
+    int z = add(a, b,c,d);
+    return e + z;
+}
